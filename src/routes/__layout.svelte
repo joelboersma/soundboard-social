75211b115e70a15a9dd38ad50155d332026d4575
Joel Boersma <joelboersma@gmail.com>
2022-07-24 17:31:03 -0700

    basic title

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -2,6 +2,8 @@
     import "../app.css";
     import Header from "../components/Header.svelte";
     import Footer from "../components/Footer.svelte";
+
+    document.title = "SoundBoard Social"
 </script>
 
 <Header />
